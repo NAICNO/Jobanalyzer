5e50b42af4676b310e4386ab645150204cef5dc1
Lars T Hansen <larstha@uio.no>
2023-10-19 16:00:55 +0200

    No bug: More install instructions

--- install.sh
+++ install.sh
@@ -23,6 +23,9 @@ cp sonalyze/release/sonalyze $SONARDIR
 cp naicreport/naicreport $SONARDIR
 cp loginfo/loginfo $SONARDIR
 cp production/ml-nodes/ml-nodes.json production/ml-nodes/*.sh production/ml-nodes/*.cron $SONARDIR
+cp key-to-web-server.pem $SONARDIR
+
+echo "You need to edit upload-data.sh and webload-5m-and-upload.sh to point to the key you just copied"
 
 # Set up the front-end; this may be on a different host though
 mkdir -p $WWWDIR
@@ -33,9 +36,13 @@ cp dashboard/*.html dashboard/*.js dashboard/*.css $WWWDIR
 # As for the rest...
 
 echo "On compute nodes you must setup cron or similar to run sonar:"
-echo "cd $SONARDIR ; crontab jobanalyzer.cron"
+echo "  cd $SONARDIR ; crontab jobanalyzer.cron"
 echo ""
 echo "On the analysis node you must setup cron or similar to run the analysis:"
-echo "cd $SONARDIR ; crontab jobanalyzer-moneypenny.cron"
+echo "  cd $SONARDIR"
+echo "  vi jobanalyzer-moneypenny.cron"
+echo ""
+echo "Now change the definition of HOME and save the file.  Then:"
+echo "  cd $SONARDIR ; crontab jobanalyzer-moneypenny.cron"
 echo ""
 echo "You also need to have a running webserver"
