0fbe850e8d68ae72cb1e4e9c8f71473654247986
Lars T Hansen <larstha@uio.no>
2023-11-15 14:58:47 +0100

    Fix #266 (infra) - Production scripts, generate aggregates

--- production/fox/load-1h.sh
+++ production/fox/load-1h.sh
@@ -12,20 +12,50 @@ report_dir=$sonar_dir/output
 mkdir -p $report_dir
 
 $sonar_dir/naicreport load \
-		      -sonalyze $sonar_dir/sonalyze \
-		      -config-file $sonar_dir/fox.json \
-		      -data-path $data_dir \
-		      -with-downtime \
-		      -tag daily \
-		      -hourly \
-		      -output-path $report_dir
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -with-downtime \
+                      -tag daily \
+                      -hourly \
+                      -output-path $report_dir
 
 $sonar_dir/naicreport load \
-		      -sonalyze $sonar_dir/sonalyze \
-		      -config-file $sonar_dir/fox.json \
-		      -data-path $data_dir \
-		      -with-downtime \
-		      -tag weekly \
-		      -hourly \
-		      -from 7d \
-		      -output-path $report_dir
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -with-downtime \
+                      -tag weekly \
+                      -hourly \
+                      -from 7d \
+                      -output-path $report_dir
+
+$sonar_dir/naicreport load \
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -tag fox-cpu-weekly \
+                      -hourly \
+                      -from 7d \
+                      -cluster 'c1-[5-29]' \
+                      -output-path $report_dir
+
+$sonar_dir/naicreport load \
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -tag fox-gpu-weekly \
+                      -hourly \
+                      -from 7d \
+                      -cluster 'gpu-[1-9]' \
+                      -output-path $report_dir
+
+$sonar_dir/naicreport load \
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -tag fox-int-weekly \
+                      -hourly \
+                      -from 7d \
+                      -cluster 'int-[1-4]' \
+                      -output-path $report_dir
--- production/fox/load-24h.sh
+++ production/fox/load-24h.sh
@@ -12,21 +12,21 @@ report_dir=$sonar_dir/output
 mkdir -p $report_dir
 
 $sonar_dir/naicreport load \
-		      -sonalyze $sonar_dir/sonalyze \
-		      -config-file $sonar_dir/fox.json \
-		      -data-path $data_dir \
-		      -with-downtime \
-		      -tag monthly \
-		      -daily \
-		      -from 30d \
-		      -output-path $report_dir
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -with-downtime \
+                      -tag monthly \
+                      -daily \
+                      -from 30d \
+                      -output-path $report_dir
 
 $sonar_dir/naicreport load \
-		      -sonalyze $sonar_dir/sonalyze \
-		      -config-file $sonar_dir/fox.json \
-		      -data-path $data_dir \
-		      -with-downtime \
-		      -tag quarterly \
-		      -daily \
-		      -from 90d \
-		      -output-path $report_dir
+                      -sonalyze $sonar_dir/sonalyze \
+                      -config-file $sonar_dir/fox.json \
+                      -data-path $data_dir \
+                      -with-downtime \
+                      -tag quarterly \
+                      -daily \
+                      -from 90d \
+                      -output-path $report_dir
