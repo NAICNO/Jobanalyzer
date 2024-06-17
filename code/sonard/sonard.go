86eba9f9381d70bd341111cdb11b2e87401592e5
Lars T Hansen <larstha@uio.no>
2024-06-17 10:53:40 +0200

    Clean up sonard, add signal handling

--- code/sonard/sonard.go
+++ code/sonard/sonard.go
@@ -1,7 +1,7 @@
 // Run sonar repeatedly with sensible options
 //
 // Usage:
-//   sonard [-i interval-in-seconds] [-m min-cpu-time-in-seconds] -s path-to-sonar logfile
+//   sonard [-i interval-in-seconds] [-m min-cpu-time-in-seconds] [-v] -s path-to-sonar logfile
 //
 // TODO: Figure out some way of getting --batchless right.  --batchless is right for the ML nodes but
 // would be wrong for systems running Slurm, for example.  It may be that we can look for slurm
@@ -22,83 +22,81 @@ import (
 	"path"
 	"strings"
 	"time"
+	"syscall"
 
+	"go-utils/process"
 	"go-utils/status"
 )
 
+var (
+	interval = flag.Int("i", 60, "Interval in `seconds` at which to run sonar")
+	minCpu = flag.Int("m", 30, "Minimum CPU time consumption in `seconds` for a job before sonar records it")
+	sonarName = flag.String("s", "", "Sonar executable `filename`")
+	verbose = flag.Bool("v", false, "Print informational messages")
+)
+
 func main() {
-	interval, minCpu, sonarName, logfileName, verbose := commandLine()
-	if verbose {
-		status.Default().LowerLevelTo(status.LogLevelInfo)
-	}
-
-	if minCpu >= interval {
-		minCpu = interval / 2
-		if verbose {
-			status.Infof("Adjusting -m value to %d to fit value of -i (which is %d)", minCpu, interval)
-		}
-	}
-
-	// There's an assumption here that when this process receives SIGHUP or SIGINT it will not need
-	// to catch the signal and specifically close this file; the file will be closed for it, and the
-	// data written by the subprocesses will have been written to the file properly.
-
-	logfile, err := os.OpenFile(logfileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		status.Fatalf("Could not open log file %s for appending", logfileName)
-	}
-	arguments := []string{
-		"ps",
-		"--exclude-system-jobs",
-		"--exclude-commands=bash,ssh,zsh,tmux,systemd",
-		"--min-cpu-time", fmt.Sprint(minCpu),
-		"--batchless",
-	}
-	for {
-		cmd := exec.Command(sonarName, arguments...)
-		var stderr strings.Builder
-		cmd.Stdout = logfile
-		cmd.Stderr = &stderr
-		if verbose {
-			status.Infof("Running %s %v", sonarName, arguments)
-		}
-		err := cmd.Run()
-		errout := stderr.String()
-		if err != nil || len(errout) != 0 {
-			status.Fatalf("Sonar exited with an error\n%v", errors.Join(err, errors.New(errout)))
-		}
-		time.Sleep(time.Duration(interval) * time.Second)
-	}
-}
-
-func commandLine() (
-	interval int,
-	minCpu int,
-	sonarName string,
-	logfileName string,
-	verbose bool,
-) {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] output-logfile\nOptions:\n", os.Args[0])
 		flag.PrintDefaults()
 		fmt.Fprintf(flag.CommandLine.Output(), "  output-logfile\n    \tDestination for sonar log records\n")
 	}
-	flag.IntVar(&interval, "i", 60, "Interval in `seconds` at which to run sonar")
-	flag.IntVar(&minCpu, "m", 30, "Minimum CPU time consumption in `seconds` for a job before sonar records it")
-	flag.StringVar(&sonarName, "s", "", "Sonar executable `filename`")
-	flag.BoolVar(&verbose, "v", false, "Print informational messages")
 	flag.Parse()
-
-	if interval < 5 {
-		status.Fatalf("Minimum -i value is 5 seconds, have %d", interval)
+	if *verbose {
+		status.Default().LowerLevelTo(status.LogLevelInfo)
 	}
-	if minCpu < 1 {
-		status.Fatalf("Minimum -m value is 1 second, have %d", minCpu)
+	if *sonarName == "" {
+		status.Fatalf("-s is required")
+	}
+	if *interval < 5 {
+		status.Fatalf("Minimum -i value is 5 seconds, have %d", *interval)
+	}
+	if *minCpu < 1 {
+		status.Fatalf("Minimum -m value is 1 second, have %d", *minCpu)
 	}
 	rest := flag.Args()
 	if len(rest) != 1 {
 		status.Fatalf("There must be exactly one logfile argument at the end, I see %v", rest)
 	}
-	logfileName = path.Clean(rest[0])
-	return
+	logfileName := path.Clean(rest[0])
+
+	if *minCpu >= *interval {
+		*minCpu = *interval / 2
+		if *verbose {
+			status.Infof("Adjusting -m value to %d to fit value of -i (which is %d)", minCpu, interval)
+		}
+	}
+
+	logfile, err := os.OpenFile(logfileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		status.Fatalf("Could not open log file %s for appending", logfileName)
+	}
+	defer logfile.Close()
+	arguments := []string{
+		"ps",
+		"--exclude-system-jobs",
+		"--exclude-commands=bash,ssh,zsh,tmux,systemd",
+		"--min-cpu-time", fmt.Sprint(*minCpu),
+		"--batchless",
+	}
+	go func() {
+		for {
+			cmd := exec.Command(*sonarName, arguments...)
+			var stderr strings.Builder
+			cmd.Stdout = logfile
+			cmd.Stderr = &stderr
+			if *verbose {
+				status.Infof("Running %s %v", *sonarName, arguments)
+			}
+			err := cmd.Run()
+			errout := stderr.String()
+			if err != nil || len(errout) != 0 {
+				status.Fatalf("Sonar exited with an error\n%v", errors.Join(err, errors.New(errout)))
+			}
+			time.Sleep(time.Duration(*interval) * time.Second)
+		}
+	}()
+
+	// Catch sensible signals and terminate normally.
+	process.WaitForSignal(syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT)
 }
