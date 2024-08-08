653c04b24600f30a9dcd0ffba8c35a63775db578
Lars T Hansen <larstha@uio.no>
2024-08-08 20:50:45 +0200

    Clean up heatmap code

--- adhoc-reports/heatmap/main.go
+++ adhoc-reports/heatmap/main.go
@@ -1,21 +1,31 @@
-// This produces an NxN grid (N <= 100) with relative cpu usage across x and relative memory across
-// y.  Readings from sacct data are quantized into N buckets with 1..k going into the first bucket
-// and 100-k+1..100 going into the last (values are clamped to 1..100).  Usefully, N divides 100
-// evenly.  The factors of 100 are 2,2,5,5; go wild.
+// Given some text input, extract two fields and produce a square heatmap with the values for one
+// field running along the x axis and the other down the y axis.
 //
-// A grid cell gets a point if a job falls into that bucket.
+// The default grid size (not currently overridable) is 20x20 boxes.  Input values are quantized
+// into those boxes and each box is incremented when a value falls in it.  Before quantization the
+// input values are clamped.  The min and max values for clamping are 0 and 100 ie percentages (not
+// currently overridable).
 //
-// Eg to produce a graphical heat map:
+// The number of quantities for quantization is the number of colors in the color table (currently
+// also 20 but this is accidental).
 //
-// sonalyze sacct -data-dir ~/sonalyze-test/data/fox.educloud.no -f 3d -fmt awk,default | ./heatmap -ppm > foo.ppm
+// In the heat map, boxes with zero values are displayed as white and all other values are given a
+// color from the color map; the colors are selected by dividing the count by the ratio of max cell
+// value to the number of colors.
 //
-// ppms can be opened in emacs and in image viewers, at least.  To convert to easier-to-handle png,
-// install netpbm and then run pnmtopng foo.ppm > foo.png.
 //
-// In principle the ppm format supports embedded comments to allow self-identifying files, but
-// neither emacs nor netpbm seems to understand those.
+// Eg to produce a png format heat map from the default output of `sonalyze sacct`, where the fifth
+// column (one-based) has relative cpu usage and the sixth column has relative memory usage:
+//
+// sonalyze sacct -data-dir ~/sonalyze-test/data/fox.educloud.no -f 3d -fmt awk,default | ./heatmap -a 5 -b 6 -ppm | pnmtopng > fox-default-3d.png
+//
+// (Without pnmtopng, save the ppm and view it in emacs and in image viewers.)
+//
+// The default text output displays raw non-zero cell values and is usually fine in a terminal window.
 //
 // TODO:
+//
+// - The text heatmap will not be formatted properly once a cell value goes over 999
 //
 // - Allowing the size of the grid to be specified may be useful (the size should divide 100 evenly).
 //
@@ -39,13 +49,81 @@ import (
 )
 
 const (
-	N = 20
-	width = N
-	height = N
+	N               = 20
+	width           = N
+	height          = N
 	pixelsPerSquare = 20
 )
 
-var grid [width*height]int
+var grid [width * height]int
+
+var (
+	a   = flag.Int("a", 1, "One-based index of first column")
+	b   = flag.Int("b", 2, "One-based index of second column")
+	ppm = flag.Bool("ppm", false, "Dump output in ppm format rather than text")
+)
+
+func main() {
+	flag.Parse()
+	if *a < 1 || *b < 1 {
+		fmt.Fprintf(os.Stderr, "%s: Nonsense column(s): %d %d\n", os.Args[0], *a, *b)
+		os.Exit(1)
+	}
+
+	scan := bufio.NewScanner(os.Stdin)
+	var any bool
+	var maxval int
+	for scan.Scan() {
+		any = true
+		fields := strings.Split(scan.Text(), " ")
+		if *a > len(fields) || *b > len(fields) {
+			fmt.Fprintf(os.Stderr, "%s: Nonsense column(s): %d %d\n", os.Args[0], *a, *b)
+			os.Exit(1)
+		}
+		v1, e1 := strconv.ParseUint(fields[int(*a-1)], 10, 64)
+		v2, e2 := strconv.ParseUint(fields[int(*b-1)], 10, 64)
+		if e1 != nil || e2 != nil {
+			fmt.Fprintf(os.Stderr, "%s: Unparseable field(s): '%s' '%s'\n", os.Args[0], fields[int(*a-1)], fields[int(*b-1)])
+			os.Exit(1)
+		}
+		v1 = min(100, max(1, v1))
+		v2 = min(100, max(1, v2))
+		const quant = 100 / N
+		xLoc := ((v1 - 1) / quant)
+		yLoc := ((v2 - 1) / quant)
+		grid[yLoc*width+xLoc]++
+		maxval = max(maxval, grid[yLoc*width+xLoc])
+	}
+
+	if any {
+		switch {
+		case *ppm:
+			printPpm(maxval)
+		default:
+			printText()
+		}
+	}
+}
+
+func printText() {
+	fmt.Printf(" ")
+	for x := 0; x < width; x++ {
+		fmt.Printf(" ---")
+	}
+	fmt.Println()
+	for y := 0; y < height; y++ {
+		fmt.Printf("|")
+		for x := 0; x < width; x++ {
+			v := grid[y*width+x]
+			if v == 0 {
+				fmt.Printf("    ")
+			} else {
+				fmt.Printf("%4d", v)
+			}
+		}
+		fmt.Println()
+	}
+}
 
 /* "magma" from https://waldyrious.net/viridis-palette-generator/ */
 var (
@@ -71,85 +149,35 @@ var (
 		[]byte{7, 6, 28},
 		[]byte{0, 0, 4},
 	}
-	black = []byte{0,0,0}
-	white = []byte{255,255,255}
+	black = []byte{0, 0, 0}
+	white = []byte{255, 255, 255}
 )
 
-var (
-	ppm = flag.Bool("ppm", false, "Dump output in ppm format rather than text")
-)
-
-func main() {
-	flag.Parse()
-
-	scan := bufio.NewScanner(os.Stdin)
-	var errs int
-	var any bool
-	var maxval int
-	for scan.Scan() {
-		any = true
-		fields := strings.Split(scan.Text(), " ")
-		cpu, e1 := strconv.ParseUint(fields[4], 10, 64)
-		mem, e2 := strconv.ParseUint(fields[5], 10, 64)
-		if e1 != nil || e2 != nil {
-			errs++
-			continue
-		}
-		cpu = min(100, max(1, cpu))
-		mem = min(100, max(1, mem))
-		const quant = 100/N
-		cpuLoc := ((cpu-1) / quant)
-		memLoc := ((mem-1) / quant)
-		grid[memLoc*width+cpuLoc]++
-		maxval = max(maxval, grid[memLoc*width+cpuLoc])
+func printPpm(maxval int) {
+	q := float64(maxval) / float64(len(colors)-1)
+	realWidth := width*pixelsPerSquare + 2
+	realHeight := height*pixelsPerSquare + 2
+	fmt.Printf("P6 %d %d 255\n", realWidth, realHeight)
+	for x := 0; x < realWidth; x++ {
+		os.Stdout.Write(black)
 	}
-
-	if any {
-		if *ppm {
-			q := float64(maxval) / float64(len(colors)-1)
-			realWidth := width*pixelsPerSquare+2
-			realHeight := height*pixelsPerSquare+2
-			fmt.Printf("P6 %d %d 255\n", realWidth, realHeight)
-			for x := 0 ; x < realWidth ; x++ {
-				os.Stdout.Write(black)
-			}
-			for y := 0 ; y < height ; y++ {
-				for j := 0 ; j < pixelsPerSquare ; j++ {
-					os.Stdout.Write(black)
-					for x := 0 ; x < width ; x++ {
-						val := grid[y*width+x]
-						var c = white
-						if val > 0 {
-							c = colors[int(math.Floor(float64(val)/q))]
-						}
-						for i := 0 ; i < pixelsPerSquare ; i++ {
-							os.Stdout.Write(c)
-						}
-					}
-					os.Stdout.Write(black)
+	for y := 0; y < height; y++ {
+		for j := 0; j < pixelsPerSquare; j++ {
+			os.Stdout.Write(black)
+			for x := 0; x < width; x++ {
+				val := grid[y*width+x]
+				var c = white
+				if val > 0 {
+					c = colors[int(math.Floor(float64(val)/q))]
+				}
+				for i := 0; i < pixelsPerSquare; i++ {
+					os.Stdout.Write(c)
 				}
 			}
-			for x := 0 ; x < realWidth ; x++ {
-				os.Stdout.Write(black)
-			}
-		} else {
-			fmt.Printf(" ")
-			for x := 0 ; x < width ; x++ {
-				fmt.Printf(" ---")
-			}
-			fmt.Println()
-			for y := 0 ; y < height ; y++ {
-				fmt.Printf("|")
-				for x := 0 ; x < width ; x++ {
-					v := grid[y*width+x]
-					if v == 0 {
-						fmt.Printf("    ")
-					} else {
-						fmt.Printf("%4d", v)
-					}
-				}
-				fmt.Println()
-			}
+			os.Stdout.Write(black)
 		}
+	}
+	for x := 0; x < realWidth; x++ {
+		os.Stdout.Write(black)
 	}
 }
--- adhoc-reports/heatmap/std.sh
+++ adhoc-reports/heatmap/std.sh
@@ -5,10 +5,10 @@
 SONALYZE=${SONALYZE:-../../code/sonalyze/sonalyze}
 
 $SONALYZE sacct -data-dir ~/sonalyze-test/data/fox.educloud.no -min-elapsed 7200 -min-reserved-mem 20 -min-reserved-cores 32 -f 3d -fmt awk,default \
-    | ./heatmap -ppm \
+    | ./heatmap -ppm -a 5 -b 6 \
     | pnmtopng > fox-2h-20G-32cpu-3d.png
 
 $SONALYZE sacct -data-dir ~/sonalyze-test/data/fox.educloud.no -min-elapsed 3600 -min-reserved-mem 10 -min-reserved-cores 16 -f 3d -fmt awk,default \
-    | ./heatmap -ppm \
+    | ./heatmap -ppm -a 5 -b 6 \
     | pnmtopng > fox-1h-10G-16cpu-3d.png
 
