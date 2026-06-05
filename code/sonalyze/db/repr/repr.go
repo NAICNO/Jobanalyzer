82928b141e521ba71e0bdfbaeaf0c2e80448a5f8
Lars T Hansen <larstha@uio.no>
2026-06-05 09:36:43 +0200

    No bug: Minor refactor

--- code/sonalyze/db/repr/repr.go
+++ code/sonalyze/db/repr/repr.go
@@ -16,3 +16,11 @@ func init() {
 	var s string
 	StringSize = unsafe.Sizeof(s)
 }
+
+// `Filterable` is implemented by data representations that can be filtered by the standard
+// ApplyFilter function in data/common/filter.go.  The timeVal must be either an RFC3339 timestamp
+// string or a time.Time value.  If a string, it will be parsed into a time value.
+
+type Filterable interface {
+	TimeAndNode() (timeVal any, nodeName string)
+}
