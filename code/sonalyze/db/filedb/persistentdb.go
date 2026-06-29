97433f8c84d1a607ca7a2e07209dfcb54f18dc9a
Lars T Hansen <larstha@uio.no>
2026-06-29 08:42:18 +0200

    No bug: Move the filename globber

--- code/sonalyze/db/filedb/persistentdb.go
+++ code/sonalyze/db/filedb/persistentdb.go
@@ -61,6 +61,7 @@ import (
 	"sync"
 	"time"
 
+	"go-utils/hostglob"
 	uslices "go-utils/slices"
 	. "sonalyze/common"
 	"sonalyze/db/errs"
@@ -691,7 +692,7 @@ func (pc *PersistentCluster) findFilesLocked(
 		// Retain only files whose names match the filter, if present
 
 		if !hosts.IsEmpty() {
-			matcher := hosts.FilenameGlobber(globs)
+			matcher := FilenameGlobber(hosts, globs)
 			for _, c := range fa.getFiles(d) {
 				if matcher.Match(c.Basename) {
 					files = append(files, c)
@@ -705,6 +706,28 @@ func (pc *PersistentCluster) findFilesLocked(
 	}
 
 	return files
+}
+
+// Create a new globber from the patterns by first inserting the host patterns into the globs and
+// then creating the globber from that.  The globs are file name patterns with exactly one *.
+// FilenameGlobber expands those patterns into sets of file name patterns by substituting the host
+// name patterns that are in this Hosts object for the *, and returns a matcher that can be used to
+// match against file names.
+func FilenameGlobber(h Hosts, globs []string) *hostglob.HostGlobber {
+	globbers := make([]*hostglob.HostGlobber, 0)
+	for _, glob := range globs {
+		if strings.Count(glob, "*") != 1 {
+			panic("Host glob must have exactly one '*'")
+		}
+		before, after, _ := strings.Cut(glob, "*")
+		globber, err := hostglob.NewGlobberWithFix(true, before, h.Patterns(), after)
+		if err != nil {
+			panic("Host glob compilation should not have failed")
+		}
+		globbers = append(globbers, globber)
+	}
+	globber := hostglob.Join(globbers)
+	return globber
 }
 
 // Pre: LOCK HELD
