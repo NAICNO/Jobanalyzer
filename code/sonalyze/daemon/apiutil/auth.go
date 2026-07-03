1be5e3c41b964bb9ea6376e9a4c44bbec5c68575
Lars T Hansen <larstha@uio.no>
2026-07-03 09:17:26 +0200

    For #891 - restructure REST API

--- code/sonalyze/daemon/apiutil/auth.go
+++ code/sonalyze/daemon/apiutil/auth.go
@@ -5,15 +5,19 @@ package apiutil
 import (
 	"encoding/base64"
 	"strings"
+
+	"github.com/danielgtaylor/huma/v2"
+
+	"go-utils/auth"
 )
 
 type AuthHeader struct {
 	Auth string `header:"Authorization"`
 }
 
-func DecodeAuth(auth string) (string, string) {
-	if strings.HasPrefix(auth, "Basic ") {
-		data, err := base64.StdEncoding.DecodeString(strings.TrimSpace(auth[6:]))
+func DecodeAuth(authHdr string) (string, string) {
+	if strings.HasPrefix(authHdr, "Basic ") {
+		data, err := base64.StdEncoding.DecodeString(strings.TrimSpace(authHdr[6:]))
 		if err == nil {
 			user, pass, found := strings.Cut(string(data), ":")
 			if found {
@@ -22,4 +26,14 @@ func DecodeAuth(auth string) (string, string) {
 		}
 	}
 	return "-NO-USER-", "-NO-PASS-"
+}
+
+func CheckAuth(command string, authenticator *auth.Authenticator, authHdr string) huma.StatusError {
+	if authenticator != nil {
+		user, pass := DecodeAuth(authHdr)
+		if !authenticator.Authenticate(user, pass) {
+			return huma.Error401Unauthorized(command + ": Unknown user/pass combination")
+		}
+	}
+	return nil
 }
